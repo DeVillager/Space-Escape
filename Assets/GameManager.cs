6f575cb4e20afd2e7a45f5bdf02963f12bfe6717
Tommi M <tommi.m@hotmail.com>
2020-10-18 16:49:09 +0300

    loading UI additive

---
+++ Assets/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+}
