63c2551b1f79c9ef75f5bc6ea51d036373dfed63
Tommi M <tommi.m@hotmail.com>
2020-10-18 16:18:12 +0300

    main menu core, level splashscreen in UI

---
+++ Assets/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : Singleton<UIManager>
+{
+    public GameObject levelLoadScreen;
+    public Animator anim;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        levelLoadScreen.SetActive(true);
+        // ShadeOut();
+    }
+
+    private void OnEnable()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ShadeOut();
+    }
+
+    private void ShadeOut()
+    {
+        // anim.SetTrigger("ShadeOut");
+    }
+
+    private void ShadeIn()
+    {
+    }
+}
